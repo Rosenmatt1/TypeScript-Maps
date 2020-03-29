bc87788d7b516ca3df226e33127e90f2576dc9ec
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-03-29 15:31:48 -0600

    Added markers for the location and a infoWIwindow event listener

---
+++ src/CustomMap.ts
@@ -0,0 +1,43 @@
+import { User } from './User';
+import { Company } from './Company';
+
+// Instructions to every other class on how they can be an argument to addMarker
+
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  }
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divID: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divID), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+
+  addMarker(mappable: Mappable ): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    })
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Hi there'
+      })
+      infoWindow.open(this.googleMap, marker)
+    })
+  }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-// import { User } from './User';
-// import { Company } from './Company';
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+
+const user = new User();
+const company = new Company()
+const customMap = new CustomMap('map');
+
+customMap.addMarker(user)
+customMap.addMarker(company)
 
 // const user = new User();
 // const company = new Company();
@@ -7,11 +15,4 @@
 // console.log(user);
 // console.log(company);
 
-const map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-});
 
